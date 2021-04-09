6c0ebf8c78674e6db4366d43d6ca88f7e08e4192
SANGO Omer <omersango777@gmail.com>
2021-04-09 15:14:43 +0000

    Add control access for account routes

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Security;
@@ -108,5 +109,19 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
     protected function getLoginUrl()
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+
+    /**
+    * Overrise to control what happens when the user hits a secure page
+    * but isn't Logged in yet.
+    *
+    * @return RedirectResponse
+    */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        $request->getSession()->getFlashBag()->add('error', 'You need to log in first !');
+        $url = $this->getLoginUrl();
+
+        return new redirectResponse($url);
     }
 }
